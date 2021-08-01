66504984eb716c907623b8063c5331ff755ddc4c
madcypher13 <88271508+madcypher13@users.noreply.github.com>
2021-08-01 18:53:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 const CONFIG = {
     introTitle: 'hello babyyy',
     introDesc: `Đà Lạt hôm nay sương mù bao phủ
-           nhớ em rùi biết ngủ làm sao 🥺🥺🥺🥺🥺
-    
-                          `,
+           nhớ em rùi biết ngủ làm sao 🥺🥺🥺 `,
     btnIntro: 'gúm',
     title: 'Do dịch quó nên mình hom đi chơi sau khi anh thi được nhưng mà happy our anniversary! Yayyyy😙',
-    desc: 'Happy our 9 thánggggg ',
-    btnYes: 'Nay học ai sến rện z ',
+    desc: 'Happy our 9 thánggggg',
+    btnYes: 'Nay học ai sến rện z',
     btnNo: 'em cũng muốn nói là happy our 9 thénggggg',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -15,5 +13,5 @@ const CONFIG = {
     mess: 'Anh biết anh đệp mà 🥰. Anh cũng yêu em bé lúmmmm 😘😘',
     messDesc: 'hẹn sau dịch mình sẽ đi hẹn hò nhé <33333.Anh đã lên kế hoạch rùiiii',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/kir.tran.3' 
+    messLink: 'https://www.facebook.com/kir.tran.3', 
 }
