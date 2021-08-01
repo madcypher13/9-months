13e326c3e8147491f5dc23c9f365dc60d1a06ed4
madcypher13 <88271508+madcypher13@users.noreply.github.com>
2021-08-01 18:06:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'hello babyyy',
+    introDesc: `Đà Lạt hôm nay sương mù bao phủ
+           nhớ em rùi biết ngủ làm sao 🥺🥺🥺🥺🥺
+    
+                          `,
+    btnIntro: 'gúm',
+    title: 'Do dịch quó nên mình hom đi chơi sau khi anh thi được nhưng mà happy our anniversary! Yayyyy😙',
+    desc: 'Happy our 9 thánggggg ',
+    btnYes: 'Nay học ai sến rện z ',
+    btnNo: 'em cũng muốn nói là happy our 9 thénggggg',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Do Nhật Hoàng siu cấp đẹp trai dễ thương mún xỉu đóaaaa <33333333',
+    mess: 'Anh biết anh đệp mà 🥰. Anh cũng yêu em bé lúmmmm 😘😘',
+    messDesc: 'hẹn sau dịch mình sẽ đi hẹn hò nhé <33333.Anh đã lên kế hoạch rùiiii',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/kir.tran.3' 
 }
